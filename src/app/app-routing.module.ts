3c5eba53b138c352d9ba6308cd6acd69df893734
Eduardo García <eduardo.gl24@hotmail.com>
2022-08-03 07:19:07 -0500

    add vehicle details ssection

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EvaluateComponent } from './pages/evaluate/evaluate.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'evaluate', component: EvaluateComponent },
+  { path: '',   redirectTo: '/evaluate', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
