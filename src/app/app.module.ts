f8edad41e52b829eb3bfc7e68d33c86f2932e28f
Eduardo García <eduardo.gl24@hotmail.com>
2022-08-03 01:36:52 -0500

    add menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { EvaluateComponent } from './pages/evaluate/evaluate.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MenuComponent,
+    EvaluateComponent
   ],
   imports: [
     BrowserModule,
